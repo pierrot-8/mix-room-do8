a4b77a89851499f24faec6d303ca737ed956e996
pierrot-8 <pierrot-do8@yahoo.co.jp>
2014-05-24 16:36:50 +0900

    douya

---
+++ same.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+

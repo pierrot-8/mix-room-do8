8790f96e5ddc2049c7983b44efae0784fd2e6e1b
pierrot-8 <pierrot-do8@yahoo.co.jp>
2014-05-27 04:23:01 +0900

    (hello world)*10 part4

---
+++ same4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	a = 1;
+
+	switch (a)
+	{
+	case 1:
+		printf("hello world four\n");
+	case 2:
+		printf("hello world four\n");
+	case 3:
+		printf("hello world four\n");
+	case 4:
+		printf("hello world four\n");
+	case 5:
+		printf("hello world four\n");
+	case 6:
+		printf("hello world four\n");
+	case 7:
+		printf("hello world four\n");
+	case 8:
+		printf("hello world four\n");
+	case 9:
+		printf("hello world four\n");
+	case 10:
+		printf("hello world four\n");
+
+		break;
+	}
+	return 0;
+}
